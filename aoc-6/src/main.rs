087129a36d9b8115870ac91a245ea3173322304b
eveningsteps <eveningsteps@gmail.com>
2021-12-13 10:41:45 +0100

    add aoc-6 pt2 (duh dynamics)

--- aoc-6/src/main.rs
+++ aoc-6/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::fmt;
 
 use structopt::StructOpt;
 
@@ -10,72 +9,38 @@ struct Cli {
     days: i32,
 }
 
-#[derive(Debug, Clone)]
-struct Lanternfish {
-    pub age: i32
-}
-
-impl fmt::Display for Lanternfish {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.age)
-    }
-}
-
-impl Lanternfish {
-    pub fn new() -> Self {
-        Self {
-            age: 8
-        }
-    }
-
-    pub fn reset(&mut self) {
-        self.age = 6;
-    }
-
-    pub fn simulate(&mut self) -> Option<Self> {
-        match self.age {
-            0 => {
-                self.reset();
-                Some(Self::new())
-            }
-            _ => {
-                self.age -= 1;
-                None
-            }
-        }
-    }
-}
-
-fn read_input() -> Vec<Lanternfish> {
+fn read_input() -> Vec<i32> {
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).unwrap();
-    buf.trim().split(",").map(|s| {
-        let age: i32 = s.parse().unwrap();
-        Lanternfish { age }
+    buf.trim().split(",").map(|s| -> i32 {
+        s.parse().unwrap()
     }).collect()
 }
 
-fn simulate(v: &Vec<Lanternfish>, days: i32) -> i32 {
-    let mut fish = v.to_vec();
-    for d in 0..days {
-        // println!("Simulating day {}", d);
-        let mut new_fish = Vec::<Lanternfish>::new();
-        for f in fish.iter_mut() {
-            if let Some(l) = (*f).simulate() {
-                new_fish.push(l);
-            }
-        }
-        fish.append(&mut new_fish);
-        // println!("Day {}: {}", d, fish.iter().map(|f| -> String {
-        //     f.to_string()
-        // }).collect::<Vec<String>>().join(","));
+
+fn dynamic(v: &Vec<i32>, days: i32) -> i64 {
+    let mut fish: Vec<Vec<i64>> = Vec::new();
+    let max_fish_lifetime = 8;
+    for _ in 0..days {
+        fish.push(vec![0; max_fish_lifetime + 1]);
     }
-    fish.len() as i32
+    for f in v {
+        fish[0][*f as usize] += 1;
+    }
+
+    for day in 1..(days as usize) {
+        for i in (0..max_fish_lifetime).rev() {
+            fish[day][i] = fish[day - 1][i + 1];
+        }
+        fish[day][max_fish_lifetime] += fish[day - 1][0];
+        fish[day][6] += fish[day - 1][0];
+    }
+    fish.last().unwrap().iter().sum()
 }
 
 fn main() {
     let args = Cli::from_args();
     let input = read_input();
-    let answer = simulate(&input, args.days);
+    let answer = dynamic(&input, args.days + 1);
     println!("{}", answer);
 }
